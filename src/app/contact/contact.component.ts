b769d0b705b2a80546c26f1be22965550014f92f
Valerie Linares <vlinares.dev@gmail.com>
2018-07-25 17:37:21 +0200

    home page css fixed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../api/auth.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private myAuthServ: AuthService
+  ) { }
 
   ngOnInit() {
   }
